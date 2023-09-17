3e3a72fe41601a669d2dd980e3a4a5525b80deee
Doniyor Doniyorov <ddonierov96@gmail.com>
2023-09-17 10:14:12 +0500

    init

--- src/main/java/com/spring/jwt/controllers/MainController.java
+++ src/main/java/com/spring/jwt/controllers/MainController.java
@@ -3,7 +3,6 @@ package com.spring.jwt.controllers;
 import java.security.Principal;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
